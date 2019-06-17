b5c3593f7b38481767bd052ac5ee8f23f074f217
Vinicius Pons <viniciusdev@zoho.com>
2019-06-17 17:14:56 -0300

    base google

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-app.get("/", (rec, res) => {
-    res.send({ bye: "buddy" });
-});
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback"
+        },
+        async (token, tokenSecret, profile, done) => {
+            console.log(token);
+            await done(profile);
+        }
+    )
+);
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
