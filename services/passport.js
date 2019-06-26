27e7ebf356c7bb228287624779de4af29695a029
ponsdev <viniciusdev@zoho.com>
2019-06-26 01:27:26 -0300

    ok

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: "/auth/google/callback"
+            callbackURL: "/auth/google/callback",
+            proxy: true
         },
         (token, tokenSecret, profile, done) => {
             User.findOne({ googleId: profile.id }).then(existingUser => {
