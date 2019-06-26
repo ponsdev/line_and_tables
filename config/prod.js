4a7f78020cd12901e37ad8c467c3053793e62671
ponsdev <viniciusdev@zoho.com>
2019-06-26 01:33:36 -0300

    dev env ok

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//     googleClientID:
-//         "415814595200-kam5qn5o9un1qhbo27e06ed5dgv7h9om.apps.googleusercontent.com",
-//     googleClientSecret: "xo_-cyhgY-Bn_el45FRhTlj7",
-//     mongoURI:
-//         "mongodb+srv://admin:D5jxMuHGe6Ly8Wr3>@cluster0-fbiqj.mongodb.net/test?retryWrites=true&w=majority",
-//     cookieKey: "sdlafasfsa8161816*"
-// };
-
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
